b0c15e5bb0ffdc9e12b6e583c8f7d6d132399858
http://ī.am/levi-xd×͜×​᭄ <111663350+mytobot@users.noreply.github.com>
2022-10-26 20:00:27 +0800

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,11 +1,12 @@
 let handler = async (m, { conn, usedPrefix: _p, __dirname, args }) => {
 let text = `
 ┌─「 Donasi • Pulsa 」
-│ • *Indosat:* [${global.ppulsa}]
+│ • *Byu:* [${global.ppulsa}]
 ❏────
 
 ┌─「 Donasi • Non Pulsa 」
 │ • *Dana:* [${global.pdana}]
+│ • *Ovo:* [085172446692]
 ❏────
 `
 
